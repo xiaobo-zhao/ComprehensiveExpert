b63c7aa9f6eaba1defae4217c00823f326ca6c76
xiaobo-zhao <334517371@qq.com>
2022-11-21 20:12:38 +0800

    管理员部分登录注册完成，向前端传递json数据格式

--- src/main/java/com/example/demo/mapper/ManagerMapper.java
+++ src/main/java/com/example/demo/mapper/ManagerMapper.java
@@ -2,6 +2,7 @@ package com.example.demo.mapper;
 
 import com.example.demo.pojo.Manager;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Mapper
@@ -9,4 +10,7 @@ import org.springframework.stereotype.Repository;
 public interface ManagerMapper {
     //    添加管理员信息
     public void addManager(Manager manager);
+
+    //    查询管理员信息
+    public Integer selectManager(@Param(value = "musername") String musername, @Param(value = "mpassword") String mpassword);
 }
